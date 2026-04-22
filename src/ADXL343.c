7ed3d78fd0ab756eb735c1144ff83ffc0c69e39a
Abhirath Koushik <abhirath.koushik@colorado.edu>
2026-04-22 09:53:54 -0600

    Refactor fall detection logic and remove unused code

--- src/ADXL343.c
+++ src/ADXL343.c
@@ -5,7 +5,6 @@
  *      Author: abhir
  */
 
-
 #include "ADXL343.h"
 #define INCLUDE_LOG_DEBUG 1
 #include "log.h"
@@ -70,11 +69,13 @@ void process_ADXL343_values(void)
 {
     /* Read INT_SOURCE and confirms freefall */
     uint8_t src = adxl_read_register(ADXL343_REG_INT_SOURCE);
+    uint8_t fall_detected_flag;
     if (src & INT_SRC_FREEFALL) {
         LOG_INFO("*** FREEFALL DETECTED! ***\n\r");
+        fall_detected_flag = 0x01;
+    } else {
+        fall_detected_flag = 0x00;
     }
-
-    src=0x10;
 
     // -------------------------------
     // Write our local GATT DB
@@ -82,24 +83,23 @@ void process_ADXL343_values(void)
     sl_status_t sc = sl_bt_gatt_server_write_attribute_value(
         gattdb_Fall_characteristic,
         0,
-        sizeof(src),
-        &src);
+        sizeof(fall_detected_flag),
+        &fall_detected_flag);
+
     if (sc != SL_STATUS_OK)
     {
         LOG_ERROR("write_attribute_value failed: 0x%04x\n\r", (unsigned int)sc);
     }
 
-    LOG_INFO("Sending indications now  for fall detection \n \r");
     ble_data_struct_t *ble = getBleDataPtr();
-    if (ble->connectionOpen &&
-        ble->FallDetection_Indications_Enabled)
+    if (fall_detected_flag && ble->connectionOpen &&
+        ble->FallDetection_Indications_Enabled && !ble->is_Indication_Inflight)
     {
-        LOG_INFO("Sending indications now  for fall detection inside  2 \n \r");
         sc = sl_bt_gatt_server_send_indication(
             ble->connectionHandle,
             gattdb_Fall_characteristic, // handle from gatt_db.h
-            sizeof(src),
-            &src);
+            sizeof(fall_detected_flag),
+            &fall_detected_flag);
         if (sc != SL_STATUS_OK)
         {
             LOG_ERROR("sl_bt_gatt_server_send_indication failed with status code of status=0x%04x  \n \r", (unsigned int)sc);
@@ -110,24 +110,4 @@ void process_ADXL343_values(void)
             ble->is_Indication_Inflight = true;
         }
     }
-}
-
-
-
-
-
-
-
-uint8_t adxl_read_register(uint8_t reg)
-{
-    uint8_t val = 0;
-    adxl_tx_buf[0] = reg;
-    transferSequence.addr        = ADXL343_I2C_ADDR << 1;
-    transferSequence.flags       = I2C_FLAG_WRITE_READ;
-    transferSequence.buf[0].data = adxl_tx_buf;
-    transferSequence.buf[0].len  = 1;
-    transferSequence.buf[1].data = &val;
-    transferSequence.buf[1].len  = 1;
-    I2CSPM_Transfer(I2C0, &transferSequence);
-    return val;
 }
